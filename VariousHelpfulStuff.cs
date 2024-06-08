eb4cd55653028bf5c86205620f0a3f3a9d0e570b
SERDCEED24 <heraklakra@gmail.com>
2024-06-09 02:38:09 +0500

    Add project files.

---
+++ VariousHelpfulStuff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsLibrary
+{
+    public class VHS
+    {
+        public static int Input(string errorText, Predicate<int>? condition = null)
+        {
+            int x;
+            string? buf;
+            bool isConvert;
+            do
+            {
+                buf = Console.ReadLine();
+                isConvert = int.TryParse(buf, out x);
+                if (condition != null)
+                {
+                    if (!condition(x))
+                    {
+                        isConvert = false;
+                    }
+                }
+                if (!isConvert)
+                {
+                    Console.WriteLine(errorText);
+                }
+            } while (!isConvert);
+            return x;
+        }
+    }
+}
